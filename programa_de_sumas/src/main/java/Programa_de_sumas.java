d16b4f4d255b5fbd490257da8856294db64b1911
Jofre Montes <Alexissaravia159@gmail.com>
2024-04-15 21:54:40 -0500

    Agregue metodos de suma

--- programa_de_sumas/src/main/java/Programa_de_sumas.java
+++ programa_de_sumas/src/main/java/Programa_de_sumas.java
@@ -1,14 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-/**
- *
- * @author Alexis Montes
- */
 public class Programa_de_sumas {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int num1, num2, total;
+        num1 = 10;
+        num2 = 20;
+        total = num1 + num2;
+        
     }
 }
