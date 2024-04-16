fe7cae8de396b6f087186ed6707a40da7c4c9080
Jofre Montes <Alexissaravia159@gmail.com>
2024-04-15 21:47:34 -0500

    Subo la base del proyecto

---
+++ programa_de_sumas/src/main/java/Programa_de_sumas.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Alexis Montes
+ */
+public class Programa_de_sumas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
